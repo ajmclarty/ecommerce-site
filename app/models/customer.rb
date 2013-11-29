43fe70a99d769b0a5aff260fd951b28dfc9adb47
ajmclarty <ajmclarty@gmail.com>
2013-11-29 12:07:37 -0600

    Update products

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
   attr_accessible :address, :city, :name, :postal_code, :province
   validates :name, :address, :city, :postal_code, :province, :presence => true
-  belong_to :province
+  belongs_to :province
   has_many :orders
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price, :quantity
+  attr_accessible :description, :name, :price, :quantity, :category_id
   validates :description, :name, :price, :quantity, :presence => true
   validates :name, :uniqueness => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-  validate :stock, :numericality => {:greater_than_or_equal_to => 0} 
+  validate :quantity, :numericality => {:greater_than_or_equal_to => 0} 
   has_many :line_items
   belongs_to :category
 end
