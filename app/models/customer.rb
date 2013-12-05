62674b6554e3644d87215e63fe2bee8375b5e190
ajmclarty <ajmclarty@gmail.com>
2013-12-05 13:32:08 -0600

    added validateion

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ActiveRecord::Base
   attr_accessible :address, :city, :email, :first_name, :last_name, :postal_code, :province_id
+  validates :first_name, :presence => true
   belongs_to :province
   has_many :orders
 end
